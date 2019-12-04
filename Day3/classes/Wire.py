cfb6afb04e6a4a830c8811ff26bc352b7bbd6186
Dominic Orme <dominic@builderstorm.com>
2019-12-04 14:12:04 +0000

    Completed day 3

--- Day3/classes/Wire.py
+++ Day3/classes/Wire.py
@@ -9,13 +9,38 @@ class Coord:
     def to_string(self):
         return str(self.X) + "," + str(self.Y)
 
+    def distance_from(self, x, y):
+        return (abs(self.X) - abs(x)) + (abs(self.Y) - abs(y))
+
 
 class Line:
     @staticmethod
     def intersect(line_one_start: Coord, line_one_end: Coord, line_two_start: Coord, line_two_end: Coord):
-        x = line_one_start.to_string() + "->" + line_one_end.to_string()
-        y = line_two_start.to_string() + "->" + line_two_end.to_string()
-        return x + " vs " + y
+        if line_one_start.X != line_one_end.X:
+            # line_one is horizontal
+            # Make lines go outward from the origin regardless
+            if abs(line_one_start.X) > abs(line_one_end.X):
+                x = line_one_start
+                line_one_start = line_one_end
+                line_one_end = x
+
+            if abs(line_two_start.Y) > abs(line_two_end.Y):
+                y = line_two_start
+                line_two_start = line_two_end
+                line_two_end = y
+
+            cross_in_x = line_one_start.X <= line_two_start.X <= line_one_end.X or \
+                line_one_start.X >= line_two_start.X >= line_one_end.X
+            cross_in_y = line_two_start.Y <= line_one_start.Y <= line_two_end.Y or \
+                line_two_start.Y >= line_one_start.Y >= line_two_end.Y
+
+            if cross_in_x and cross_in_y:
+                return Coord(line_two_start.X, line_one_start.Y)
+            else:
+                return Coord(0, 0)
+        else:
+            val = Line.intersect(line_two_start, line_two_end, line_one_start, line_one_end)
+            return val
 
     @staticmethod
     def orthogonal(line_one_start: Coord, line_one_end: Coord, line_two_start: Coord, line_two_end: Coord):
@@ -23,9 +48,29 @@ class Line:
         h_vs_v = line_one_start.X != line_one_end.X and line_two_start.X == line_two_end.X
         return h_vs_v or v_vs_h
 
+    @staticmethod
+    def to_string(line_start, line_end):
+        if line_start.X == line_end.X:
+            return "(" + str(line_start.X) + ", " + str(line_start.Y) + "->" + str(line_end.Y) + ")"
+        else:
+            return "(" + str(line_start.X) + "->" + str(line_end.X) + ", " + str(line_start.Y) + ")"
+
+    @staticmethod
+    def contains_point(line_start, line_end, coord):
+        if line_start.X == line_end.X == coord.X:
+            return line_start.Y < coord.Y < line_end.Y or line_start.Y > coord.Y > line_end.Y
+        elif line_start.Y == line_end.Y == coord.Y:
+            return line_start.X < coord.X < line_end.X or line_start.X > coord.X > line_end.X
+        return False
+
+    @staticmethod
+    def length(line_start, line_end):
+        return abs(line_start.X - line_end.X) + abs(line_start.Y - line_end.Y)
+
 
 class Wire:
-    Locations = []
+    def __init__(self):
+        self.Locations = []
 
     def trace(self, path):
         current_x = 0
@@ -44,17 +89,35 @@ class Wire:
                 current_y -= distance
             self.Locations.append(Coord(current_x, current_y))
 
-    def intersect(self, wire: Wire):
-        i = 0
-        for j in range(0, len(wire.Locations), 2):
-            print(
-                self.Locations[i].to_string() + "->" +
-                self.Locations[i+1].to_string() + " vs. " +
-                wire.Locations[j].to_string() + "->" +
-                wire.Locations[j+1].to_string()
-            )
-            if Line.orthogonal(self.Locations[i], self.Locations[i + 1], wire.Locations[j], wire.Locations[j+1]):
-                print(Line.intersect(self.Locations[i], self.Locations[i + 1], wire.Locations[j], wire.Locations[j+1]))
+    def distance_to(self, coord: Coord):
+        distance = 0
+        for i in range(0, len(self.Locations) - 1):
+            if Line.contains_point(self.Locations[i], self.Locations[i + 1], coord):
+                length = Line.length(self.Locations[i], coord)
+                distance += length
+                break
             else:
-                print("Lines weren't orthogonal")
+                length = Line.length(self.Locations[i], self.Locations[i + 1])
+                distance += length
+        return distance
+
+
+class Grid:
+    def __init__(self):
+        self.Intersections = []
+
+    def intersect(self, wire1: Wire, wire2:Wire):
+        smallest_distance = 99999;
+        for i in range(0, len(wire1.Locations) - 1):
+            for j in range(0, len(wire2.Locations) - 1):
+                if Line.orthogonal(wire1.Locations[i], wire1.Locations[i + 1], wire2.Locations[j], wire2.Locations[j + 1]):
+                    intersect = Line.intersect(wire1.Locations[i], wire1.Locations[i + 1], wire2.Locations[j], wire2.Locations[j + 1])
+
+                    # Non-intersecting lines will return 0,0
+                    if not (intersect.X == 0 and intersect.Y == 0):
+                        self.Intersections.append(intersect)
+                        dist = intersect.distance_from(0, 0)
+                        if dist < smallest_distance:
+                            smallest_distance = dist
+        return smallest_distance
 
--- Day3/main.py
+++ Day3/main.py
@@ -1,27 +1,24 @@
-from functools import reduce
-
 from Day3.Util import Util
-from Day3.classes.Wire import Wire
-
-x = ["a", "b", "c"]
-y = ["c", "d", "e"]
-print(x)
-print(y)
-print(set(x).intersection(set(y)))
+from Day3.classes.Wire import Wire, Grid
 
 util = Util()
 values = util.parse('input.txt')
-print(values[0])
-print(values[1])
 initial_wire = Wire()
 initial_wire.trace(values[0])
 second_wire = Wire()
 second_wire.trace(values[1])
-#initial_wire.intersect(second_wire)
-print(initial_wire.Locations[0].to_string())
-print(initial_wire.Locations[1].to_string())
-print(initial_wire.Locations[2].to_string())
+grid = Grid()
+print("Closest: " + str(grid.intersect(initial_wire, second_wire)))
+a = []
+for intersection in grid.Intersections:
+    d1 = initial_wire.distance_to(intersection)
+    d2 = second_wire.distance_to(intersection)
+    a.append([intersection, d1 + d2])
 
-print(second_wire.Locations[0].to_string())
-print(second_wire.Locations[1].to_string())
-print(second_wire.Locations[2].to_string())
+min = 9999
+for x in a:
+    if x[1] < min:
+        min = x[1]
+        print("Intersection " + x[0].to_string() + " was closer")
+
+print(min)
